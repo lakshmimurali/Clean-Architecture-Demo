a400b3073352b75c47bb1df0ba6ea9c1a63a44a1
lakshmimurali <vmanikandan@zohocorp.com>
2023-09-25 10:03:14 +0530

    Changes Added.

--- src/application/reducers.js
+++ src/application/reducers.js
@@ -15,11 +15,19 @@ const todoReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_TODO:
       const newTodo = todoService.add(action.payload);
-    // ...
+      // ...
+      const updatedTodos = [...state.todos, newTodo];
+      saveTodos(updatedTodos);
+      return { todos: updatedTodos };
 
     case TOGGLE_TODO:
       const toggledId = action.payload;
       const toggledTodo = todoService.toggle(toggledId);
+      const updatedTodosToggled = state.todos.map((todo) =>
+        todo.id === toggledId ? { ...todo, completed: !todo.completed } : todo
+      );
+      saveTodos(updatedTodosToggled);
+      return { todos: updatedTodosToggled };
     // ...
 
     default:
